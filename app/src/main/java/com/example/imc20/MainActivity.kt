d6d533e48126afc5489299ffea04e5e5df2a4e97
Juliana Oliveira <jullianaoliveira.2003@gmail.com>
2021-08-30 17:00:25 -0300

    Inicio da construção do cálculo

--- app/src/main/java/com/example/imc20/MainActivity.kt
+++ app/src/main/java/com/example/imc20/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.imc20
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val editPeso = findViewById<EditText>(R.id.edit_peso)
+        val buttonCalcular = findViewById<Button>(R.id.button_calcular)
+
+        buttonCalcular.setOnClickListener {
+            Toast.makeText(this, editPeso.text.toString(), Toast.LENGTH_SHORT).show()
+        }
     }
 }
